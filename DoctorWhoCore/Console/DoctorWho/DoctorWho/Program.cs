005159fbb47bbd32c80c87d543f7e41782ec4df6
AsiaShalaldeh <arwwd2002@gmail.com>
2023-05-23 23:17:19 +0300

    Write Create/Update/Delete methods for all models

--- DoctorWhoCore/Console/DoctorWho/DoctorWho/Program.cs
+++ DoctorWhoCore/Console/DoctorWho/DoctorWho/Program.cs
@@ -91,4 +91,139 @@ void ExecuteSummariseEpisodes()
     }
 }
 
-ExecuteSummariseEpisodes();
+// ExecuteSummariseEpisodes();
+
+void CreateCompanion()
+{
+    var companion = new Companion { CompanionName = "Jonas", WhoPlayed = "Adam" };
+    _context.Companions.Add(companion);
+    _context.SaveChanges();
+}
+
+// CreateCompanion();
+
+void UpdateCompanion()
+{
+    var companion = _context.Companions.Where(c => c.CompanionId == 6).FirstOrDefault();
+    companion.CompanionName = "Jonass";
+    companion.WhoPlayed = "Tom Criuse";
+    _context.SaveChanges();
+}
+//UpdateCompanion();
+void DeleteCompanion()
+{
+    var companion = _context.Companions.Where(c => c.CompanionId == 1).FirstOrDefault();
+    _context.Companions.Remove(companion);
+    _context.SaveChanges();
+}
+//DeleteCompanion();
+
+void CreateEnemy()
+{
+    var enemy = new Enemy { EnemyName = "The King", Description = "Exterminate! Exterminate!" };
+    _context.Enemies.Add(enemy);
+    _context.SaveChanges();
+}
+//CreateEnemy();
+
+void UpdateEnemy()
+{
+    var enemy = _context.Enemies.Where(e => e.EnemyId == 1).FirstOrDefault();
+    enemy.EnemyName = "The Queen";
+    enemy.Description = "I am the Queen now";
+    _context.SaveChanges();
+}
+//UpdateEnemy();
+
+void DeleteEnemy()
+{
+    var enemy = _context.Enemies.Where(e => e.EnemyId == 6).FirstOrDefault();
+    _context.Enemies.Remove(enemy);
+    _context.SaveChanges();
+}
+//DeleteEnemy();
+
+void CreateDoctor()
+{
+    var doctor = new Doctor { DoctorNumber = 5378, DoctorName = "Will Smith", 
+        BirthDate = new DateTime(1975, 5, 10), FirstEpisodeDate = new DateTime(2020, 2, 1),
+        LastEpisodeDate = new DateTime(2020, 2, 15) };
+    _context.Doctors.Add(doctor);
+    _context.SaveChanges();
+}
+//CreateDoctor();
+
+void UpdateDoctor()
+{
+    var doctor = _context.Doctors.Where(d => d.DoctorId == 6).FirstOrDefault();
+    doctor.DoctorNumber = 3844;
+    _context.SaveChanges();
+}
+//UpdateDoctor();
+
+void DeleteDoctor()
+{
+    var doctor = _context.Doctors.Where(d => d.DoctorId == 6).FirstOrDefault();
+    _context.Doctors.Remove(doctor);
+    _context.SaveChanges();
+}
+//DeleteDoctor();
+
+void CreateAuthor()
+{
+    var author = new Author { AuthorName = "Leonardo Dicaprio" };
+    _context.Authors.Add(author);
+    _context.SaveChanges();
+}
+//CreateAuthor();
+
+void UpdateAuthor()
+{
+    var author = _context.Authors.Where(a => a.AuthorId == 6).FirstOrDefault();
+    author.AuthorName = "Leo Dicaprio";
+    _context.SaveChanges();
+}
+//UpdateAuthor();
+
+void DeleteAuthor()
+{
+    var author = _context.Authors.Where(a => a.AuthorId == 6).FirstOrDefault();
+    _context.Authors.Remove(author);
+    _context.SaveChanges();
+}
+//DeleteAuthor();
+
+void CreateEpisode()
+{
+    var episode = new Episode
+    {
+        SeriesNumber = 6,
+        EpisodeNumber = 6,
+        EpisodeType = "Regular",
+        Title = "An Unearthly Kid",
+        EpisodeDate = new DateTime(1963, 12, 23),
+        Notes = "First appearance of the Doctor and the TARDIS.",
+        AuthorId = 7,
+        DoctorId = 5
+    };
+    _context.Episodes.Add(episode);
+    _context.SaveChanges();
+}
+//CreateEpisode();
+
+void UpdateEpisode()
+{
+    var episode = _context.Episodes.Where(e => e.EpisodeId == 7).FirstOrDefault();
+    episode.EpisodeDate = new DateTime(1963, 12, 26);
+    episode.AuthorId = 5;
+    _context.SaveChanges();
+}
+//UpdateEpisode();
+
+void DeleteEpisode()
+{
+    var episode = _context.Episodes.Where(e => e.EpisodeId == 7).FirstOrDefault();
+    _context.Episodes.Remove(episode);
+    _context.SaveChanges();
+}
+//DeleteEpisode();
